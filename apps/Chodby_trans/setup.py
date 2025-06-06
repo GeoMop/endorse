a795241eef36284ab313ac20ed58b2dd808f303b
Pavel Exner <pavelexner@gmail.com>
2025-06-06 14:59:50 +0200

    Update venv as in chodby inv.

---
+++ apps/Chodby_trans/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="chodby_trans",           # The name of your package
+    version="0.1.0",             # Your package version
+    description="Chodby project - transport model inversion.",
+    author="Jan Brezina",
+    author_email="jan.brezina@tul.cz",
+    install_requires = ["PyYAML", "attrs", "numpy"],
+    packages=["Chodby_trans"],
+    package_dir={"Chodby_trans": "."},  # Map the package name to the current directory
+)
