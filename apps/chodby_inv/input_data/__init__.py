f101866949268361b7f7fb331081b93054879214
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-07-14 13:04:21 +0200

    Change to newer measurement file

--- apps/chodby_inv/input_data/__init__.py
+++ apps/chodby_inv/input_data/__init__.py
@@ -14,8 +14,8 @@ events_yaml = input_dir / "events.yaml"
 #blast_events_xlsx = input_dir / 'blast_events.xlsx'
 
 # smallest file
-piezo_measurement_file = input_dir / "piezo_2024_06_04.xlsx"
+#piezo_measurement_file = input_dir / "piezo_2024_06_04.xlsx"
 # last file
-#piezo_measurement_file = input_dir / "piezo_2025_04_08.xlsx"
+piezo_measurement_file = input_dir / "piezo_2025_04_08.xlsx"
 # configuration of the filtering sections
 # named sections of the data corresponding to interesting time intervals
