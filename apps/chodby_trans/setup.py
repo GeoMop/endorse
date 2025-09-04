906f42e83537c57a3d4f113fe352959a49008539
Jan Brezina <jan.brezina@tul.cz>
2025-09-04 18:25:54 +0200

    Simplified installs.

--- apps/chodby_trans/setup.py
+++ apps/chodby_trans/setup.py
@@ -6,7 +6,18 @@ setup(
     description="Chodby project - transport model inversion.",
     author="Jan Brezina",
     author_email="jan.brezina@tul.cz",
-    install_requires = ["PyYAML", "attrs", "numpy"],
+    install_requires = [
+        "PyYAML",
+        "attrs",
+        "numpy",
+        "dask",
+        "distributed",
+        "salib",
+        "openturns",
+        "joblib",
+        "loky",
+        "matplotlib",
+        "zarr_fuse @ git+https://github.com/GeoMop/zarr_fuse.git@main"],
     packages=["chodby_trans"],
     package_dir={"chodby_trans": "."},  # Map the package name to the current directory
 )
