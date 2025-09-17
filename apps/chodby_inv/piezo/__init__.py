70f77d29f0bdfce0b444083f61318883a6efaa87
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2025-09-17 21:29:08 +0200

    add an import to init

--- apps/chodby_inv/piezo/__init__.py
+++ apps/chodby_inv/piezo/__init__.py
@@ -1,4 +1,4 @@
 from .piezo_canonic import full_flat_df, denoised_df, excavation_epoch_df,\
     get_epoch, to_datetime
 from .wpt_model import PoroElasticSolver
-from .plots import plot_idata
+from .plots import plot_idata, get_generic_name
